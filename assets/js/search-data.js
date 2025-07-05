69d7a10a4855883505aa6e636620f6f20f6e94d9
Jaegg3rNat <71736459+Jaegg3rNat@users.noreply.github.com>
2025-07-05 12:12:58 +0000

    Deploying to gh-pages from @ Jaegg3rNat/Jaegg3rNat.github.io@081869e1683cd38d47e055e1ea464f4520a8505f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,35 +501,42 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6E%61%74%68%61%6E@%69%66%69%73%63.%75%69%62-%63%73%69%63.%65%73", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/Jaegg3rNat", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-lattes',
+        title: 'Lattes',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("http://lattes.cnpq.br/4311770430758420", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/https://orcid.org/0000-0001-9385-4459# your ORCID ID", "_blank");
+        },
+      },{
+        id: 'social-researchgate',
+        title: 'ResearchGate',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.researchgate.net/profile/Nathan De Oliveira Silvano/", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=YGPUfiYAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
